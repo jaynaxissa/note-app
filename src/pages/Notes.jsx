68144f85df85edd5b94fdd2dd5e5df08cc5678e4
jaynaxissa <jaynaxissa@gmail.com>
2023-04-08 16:32:24 +0700

    making the searchbar responsive

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
-import { Link } from "react-router-dom";
-import dummyNotes from "../dummy_notes";
 import { BsPlusLg } from "react-icons/bs";
+import { MdClose } from "react-icons/md";
+import { Link } from "react-router-dom";
+// import dummyNotes from "../dummy_notes";
+
 import NoteItem from "../components/NoteItem";
 
 const Notes = ({ notes }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilterNotes] = useState(notes);
+
+  const handleShowSearch = () => {
+    setShowSearch((prevState) => !prevState);
+  };
+
+  const handleTextChange = (e) => {
+    const newText = e.target.value;
+    setText(newText);
+    handleSearch(newText);
+  };
+
+  const handleSearch = (newText) => {
+    setFilterNotes(
+      notes.filter((note) => {
+        const matchText = newText.toLowerCase();
+        if (
+          note.title.toLowerCase().match(matchText) ||
+          note.details.toLowerCase().match(matchText)
+        ) {
+          return note;
+        }
+      })
+    );
+  };
+
   return (
     <section>
       <header className="notes__header">
-        <h2>My Notes</h2>
-        {/* <input type="text" autoFocus placeholder="keyword" /> */}
-        <button className="btn">
-          <CiSearch />
+        {!showSearch && <h2>My Notes</h2>}
+        {showSearch && (
+          <input
+            type="text"
+            onChange={handleTextChange}
+            autoFocus
+            placeholder="keyword"
+          />
+        )}
+        <button className="btn" onClick={handleShowSearch}>
+          {showSearch ? <MdClose /> : <CiSearch />}
         </button>
       </header>
       <div className="notes__container">
-        {notes.map((note) => (
+        {filteredNotes.map((note) => (
           <NoteItem key={note.id} note={note} />
         ))}
       </div>
